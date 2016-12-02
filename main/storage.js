31d958a75ec850c10147046e841869f3d71f74e8
Huang Liuhaoran <hlhr202@163.com>
2016-12-02 22:00:53 +0800

    fixed sales column, add default tenant_id

--- main/storage.js
+++ main/storage.js
@@ -1,56 +1,56 @@
-import { app} from 'electron';
+import { app } from 'electron';
 const storage = require('electron-json-storage');
 
 let config = {}
 
-let prepareStorage = ()=>{
-	return new Promise((resolve, reject)=>{
-		return storage.get('config', function(error, data) {
-			if (error) return reject(error)
+let prepareStorage = () => {
+    return new Promise((resolve, reject) => {
+        return storage.get('config', function(error, data) {
+            if (error) return reject(error)
 
-			if (!data['tenant_id']) {
-				data['tenant_id'] = 1
-			}
-			if (!data['pos_no']){
-				data['pos_no'] = 1
-			}
-			if (!data['tran_file_no']||data['tran_file_no']>9999){
-				data['tran_file_no'] = 1
-			}
-			config = data
-			storage.set('config', data, function(error){
-				if (error) return reject(error)
-				else return resolve(data)
-			})
-		});
-	})
+            if (!data['tenant_id']) {
+                data['tenant_id'] = '20044444-2504'
+            }
+            if (!data['pos_no']) {
+                data['pos_no'] = 1
+            }
+            if (!data['tran_file_no'] || data['tran_file_no'] > 9999) {
+                data['tran_file_no'] = 1
+            }
+            config = data
+            storage.set('config', data, function(error) {
+                if (error) return reject(error)
+                else return resolve(data)
+            })
+        });
+    })
 }
 
 let getConfig = () => {
-	return config
+    return config
 }
 
-let incrementFileNo = (current) =>{
-	return new Promise((resolve, reject)=>{
-		return storage.get('config', function(error, data) {
-			if (error) return reject(error)
+let incrementFileNo = (current) => {
+    return new Promise((resolve, reject) => {
+        return storage.get('config', function(error, data) {
+            if (error) return reject(error)
 
-			if (data['tran_file_no']>=9999 || current === 9999){
-				data['tran_file_no'] = 1
-			} else {
-				data['tran_file_no'] = current+1
-			}
-			config = data
-			storage.set('config', data, function(error){
-				if (error) return reject(error)
-				else return resolve(data)
-			})
-		});
-	})
+            if (data['tran_file_no'] >= 9999 || current === 9999) {
+                data['tran_file_no'] = 1
+            } else {
+                data['tran_file_no'] = current + 1
+            }
+            config = data
+            storage.set('config', data, function(error) {
+                if (error) return reject(error)
+                else return resolve(data)
+            })
+        });
+    })
 }
 
 module.exports = {
-	prepareStorage:prepareStorage,
-	getConfig:getConfig,
-	incrementFileNo:incrementFileNo
+    prepareStorage: prepareStorage,
+    getConfig: getConfig,
+    incrementFileNo: incrementFileNo
 }
--- renderer/app.js
+++ renderer/app.js
@@ -10,20 +10,24 @@ const ipc = ipcRenderer
 
 class App extends Component {
 	state = {
-		config:{},
-		count:0,
-		outputString:'',
-		data:{},
-		loaded:false
+		config: {},
+		count: 0,
+		outputString: '',
+		data: {},
+		loaded: false
 	}
 	componentWillMount() {
 		this.initIpc()
 	}
-	initIpc=()=>{
+	initIpc = () => {
 		ipc.send('get-config')
-		ipc.on('get-config', function(event, config){
-			self.setState({config:config})
-			self.setState({count:config['tran_file_no']})
+		ipc.on('get-config', function(event, config) {
+			self.setState({
+				config: config
+			})
+			self.setState({
+				count: config['tran_file_no']
+			})
 		})
 		var self = this;
 		ipc.on('open-file-dialog', function(event, response) {
@@ -32,13 +36,18 @@ class App extends Component {
 			} else {
 				let summery = response.summery
 				let data = self.getData(summery)
-				self.setState({data:data})
-				self.setState({loaded:true})
+				self.setState({
+					data: data
+				})
+				self.setState({
+					loaded: true
+				})
 				self.computeString()
 			}
 		})
 	}
-	getData= (summery) =>{
+
+	getData = (summery) => {
 		let data = {}
 		console.log(this.state)
 		data['tenant_id'] = this.state.config['tenant_id']
@@ -49,16 +58,21 @@ class App extends Component {
 		data['close_receipt'] = summery['tail']
 		data['total_receipt'] = summery['count']
 		data['gross_sales'] = summery['total']
+		data['tax'] = summery['tax']
 		data['net_sales'] = summery['total']
 		return data;
 	}
 
-	computeString =()=>{
+	computeString = () => {
 		let data = this.state.data
 		data['tran_file_no'] = this.state.count
-		this.setState({data:data})
-		let s = data['tenant_id']+'|'+data['pos_no']+'|'+data['sale_date']+'|'+data['tran_file_no']+'|'+data['open_receipt']+'|'+data['close_receipt']+'|'+data['total_receipt']+'|'+data['gross_sales']+'|'+'|'+'|'+'|'+'|'+'|'+'|'+data['net_sales']
-		this.setState({outputString:s})
+		this.setState({
+			data: data
+		})
+		let s = data['tenant_id'] + '|' + data['pos_no'] + '|' + data['sale_date'] + '|' + data['tran_file_no'] + '|' + data['open_receipt'] + '|' + data['close_receipt'] + '|' + data['total_receipt'] + '|' + data['gross_sales'] + '|' + data['tax'] + '|' + '|'+ '|' + '|' + '|' + '|' + data['net_sales']
+		this.setState({
+			outputString: s
+		})
 	}
 
 	chooseFileClick = (e) => {
@@ -67,16 +81,22 @@ class App extends Component {
 
 	saveFileClick = (e) => {
 		let data = this.state.data
-		let namePattern = data['tenant_id']+'_'+data['pos_no']+'_'+data['tran_file_no']+'_'+moment().format('YYMMDDhhmm')+'.txt'
+		let namePattern = data['tenant_id'] + '_' + data['pos_no'] + '_' + data['tran_file_no'] + '_' + moment().format('YYMMDDhhmm') + '.txt'
 		let fileNo = data['tran_file_no']
-		ipc.send('open-save-dialog',{outputString:this.state.outputString, namePattern:namePattern, fileNo:fileNo})
+		ipc.send('open-save-dialog', {
+			outputString: this.state.outputString,
+			namePattern: namePattern,
+			fileNo: fileNo
+		})
 	}
 
-	handleCountChange = (event)=>{
-		this.setState({count:event.target.value})
+	handleCountChange = (event) => {
+		this.setState({
+			count: event.target.value
+		})
 	}
 
-	handleSubmit = (event)=>{
+	handleSubmit = (event) => {
 		event.preventDefault()
 	}
 	render() {
@@ -85,30 +105,31 @@ class App extends Component {
 			<Container fluid>
 				<Container as='div' id='header-container' textAlign='center'>
 					<Header as='h2' icon>
-						<Icon name='file excel outline' />
-						POS Table Reader
+						<Icon name='file excel outline' /> POS Table Reader
 						<Header.Subheader>
-						TENANT ID: { config['tenant_id']? config['tenant_id']:null } Pos Number: { config['pos_no']? config['pos_no']:null } Current File Count: { config['tran_file_no']? config['tran_file_no']:null}
+							TENANT ID: { config['tenant_id'] ? config['tenant_id'] : null } Pos Number: { config['pos_no'] ? config['pos_no'] : null
+							} Current File Count: { config['tran_file_no'] ? config['tran_file_no'] : null }
 						</Header.Subheader>
 					</Header>
-  				</Container>
+				</Container>
 				<Divider/>
 				<Container textAlign='center'>
-					<Form onSubmit={this.handleSubmit}>
+					<Form onSubmit={ this.handleSubmit }>
 						<Form.Group widths='equal'>
-							<Form.Field label='Daily File Count' name='count' control={Input} value={count} onChange={this.handleCountChange} placeholder='Daily File Count' />
+							<Form.Field label='Daily File Count' name='count' control={ Input } value={ count } onChange={ this.handleCountChange } placeholder='Daily File Count'
+							/>
 						</Form.Group>
 						<Form.Group>
-							<Form.Field control={Button} primary onClick={ this.chooseFileClick }>Choose File</Form.Field>
-							<Form.Field control={Button} disabled={!loaded} primary onClick={ this.computeString }>Preview</Form.Field>
-							<Form.Field control={Button} disabled={!loaded} primary onClick={ this.saveFileClick }>Save</Form.Field>
+							<Form.Field control={ Button } primary onClick={ this.chooseFileClick }>Choose File</Form.Field>
+							<Form.Field control={ Button } disabled={ !loaded } primary onClick={ this.computeString }>Preview</Form.Field>
+							<Form.Field control={ Button } disabled={ !loaded } primary onClick={ this.saveFileClick }>Save</Form.Field>
 						</Form.Group>
 					</Form>
 				</Container>
 				<Divider/>
 				<Container textAlign='center'>
 					<Header as='h5'>Preview Your Data</Header>
-					{outputString?outputString:null}
+					{ outputString ? outputString : null }
 				</Container>
 				<ErrorModal ref="errorModal" />
 			</Container>
